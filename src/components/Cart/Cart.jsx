ec27881620cbb6a64b833b7f614095c555072d06
Asif Khan <sparkleasif@gmail.com>
2024-06-09 12:54:03 +0600

    product quantity and price update

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -5,14 +5,21 @@ const Cart = ({cart}) => {
     // const {cart} =props;         methode 2
 
     console.log(cart);
-
+    let quantity =0;
     let totalPrice=0;
     let totalShipping=0;
     
     for(const product of cart){
-        totalPrice=totalPrice+product.price;
+    //     if(product.quantity==0){
+    //         product.quantity=1;
+    //     // }  
+    //   or,
+    //     product.quantity = product.quantity || 1;
+
+        totalPrice=totalPrice+ product.price* product.quantity;
         totalShipping=totalShipping+ product.shipping;
-    }
+        quantity=quantity+ product.quantity;
+}
     const tax =totalPrice*7/100;
     const grand=totalPrice+totalShipping+tax;
 
@@ -20,7 +27,7 @@ const Cart = ({cart}) => {
         <div className='bg-orange-300 leading-10 p-6 sticky top-0'>
             <h6 className='font-bold text-xl bg-orange-300'>
                 Order Summary</h6>
-            <p>Selected Items: {cart.length}</p>
+            <p>Selected Items: {quantity}</p>
             <p>Total Price:$ {totalPrice} </p>
             <p>Total Shipping Carge:$ {totalShipping} </p>
             <p>Tax:$ {tax} </p>
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -34,7 +34,20 @@ const Shop = () => {
   }, [products]);
 
   const handleAddToCart = (product) => {
-    const newCart = [...cart, product];
+      let newCart= [];
+    // const newCart = [...cart, product];
+    // if product doest exist in the cart, then set quantity
+    // IF Exist update quantity by 1;
+    const exist = cart.find(pd=>pd.id === product.id);
+    if(!exist){
+      product.quantity=1;
+      newCart=[...cart, product];
+    }else{
+      exist.quantity = exist.quantity+1;
+      const remaining = cart.filter(pd=>pd.id!=product.id);
+      newCart=[...remaining,exist];
+    }
+
     setCart(newCart);
     addToDb(product.id);
   };
